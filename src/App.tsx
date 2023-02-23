ef4b9736a34e1a120a402c8310bb993ea921da7d
francisco-programmer <hades@localhost.localdomain>
2023-02-23 16:29:37 -0500

    navbar

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Nav from './components/Nav'
+
+function App() {
+
+ 
+  
+
+  return (
+    <div className="App ">
+      <Nav />
+      <p className='z-40'>francisco</p>
+     
+    </div>
+  )
+}
+
+export default App
