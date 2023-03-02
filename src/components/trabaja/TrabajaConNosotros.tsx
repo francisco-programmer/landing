8d33750a95683f1894923efdff80e8c725575dbd
francisco-programmer <franciscopalenciaa@gmail.com>
2023-03-02 16:35:20 -0500

    resolve

--- src/components/trabaja/TrabajaConNosotros.tsx
+++ src/components/trabaja/TrabajaConNosotros.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import CardEmpleo from './CardEmpleo'
-import encantados from '../../assets/Encantados de conocerte.png'
+
 
 const TrabajaConNosotros = () => {
-  const stilo = "font-poppins text-gray-900"
+  
     
   return (
     <div className='py-10   bg-encantados sm:px-36 px-5'>
       <div className=' p-10'>
 
-        <h1 className={`text-3xl text-center ${stilo} `}>Unete al mejor equipo de trabajo</h1>
+        <h1 className="text-3xl text-center font-poppins text-gray-900">Unete al mejor equipo de trabajo</h1>
         
-        <p className={stilo}>En Axios Studio Tenemos disponible las siguientes  Vacantes</p>
+        <p className="font-poppins text-gray-900">En Axios Studio Tenemos disponible las siguientes  Vacantes</p>
       </div>
         
           
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     },
     backgroundImage: {
       'luna': "url('https://github.com/francisco-programmer/landing/blob/main/src/assets/luna.jpeg?raw=true')",
-      "encantados": "url(./assets/Encantados de conocerte.png)"
+      "encantados": "url(./assets/Encantadosdeconocerte.png)"
       
     }
   },
